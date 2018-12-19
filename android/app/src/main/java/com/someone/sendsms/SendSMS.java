3f25f99841ba20aa1d8793291abf088cbb6e6c8b
Adrian Paul Matos <matos.adrianpaul@gmail.com>
2018-12-19 23:55:00 +0800

    Add dual sim support

--- android/app/src/main/java/com/someone/sendsms/SendSMS.java
+++ android/app/src/main/java/com/someone/sendsms/SendSMS.java
@@ -1,5 +1,7 @@
 package com.someone.sendsms;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -7,6 +9,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -30,76 +35,82 @@ public class SendSMS extends ReactContextBaseJavaModule {
         return "SendSMS";
     }
 
-    private void sendCallback(Integer messageId, String message){
+    private void sendCallback(Integer messageId, String message) {
         if (callback != null) {
             callback.invoke(messageId, message);
             callback = null;
         }
     }
 
-    //---sends an SMS message to another device---
+    // ---sends an SMS message to another device---
     @ReactMethod
-    public void send(final Integer messageId, String phoneNumber, String message, final Callback cb ){
+    public void send(Integer sim, final Integer messageId, String phoneNumber, String message, final Callback cb) {
 
-        try{
+        try {
 
             this.callback = cb;
             String SENT = "SMS_SENT";
             String DELIVERED = "SMS_DELIVERED";
 
-            PendingIntent sentPI = PendingIntent.getBroadcast(reactContext, 0,
-                    new Intent(SENT), 0);
+            PendingIntent sentPI = PendingIntent.getBroadcast(reactContext, 0, new Intent(SENT), 0);
 
-            PendingIntent deliveredPI = PendingIntent.getBroadcast(reactContext, 0,
-                    new Intent(DELIVERED), 0);
+            PendingIntent deliveredPI = PendingIntent.getBroadcast(reactContext, 0, new Intent(DELIVERED), 0);
 
-            //---when the SMS has been sent---
-            reactContext.registerReceiver(new BroadcastReceiver(){
+            // ---when the SMS has been sent---
+            reactContext.registerReceiver(new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context arg0, Intent arg1) {
-                    switch (getResultCode())
-                    {
-                        case Activity.RESULT_OK:
-                            sendCallback(messageId, "SMS sent");
-                            break;
-                        case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                            sendCallback(messageId, "Generic failure");
-                            break;
-                        case SmsManager.RESULT_ERROR_NO_SERVICE:
-                            sendCallback(messageId, "No service");
-                            break;
-                        case SmsManager.RESULT_ERROR_NULL_PDU:
-                            sendCallback(messageId, "Null PDU");
-                            break;
-                        case SmsManager.RESULT_ERROR_RADIO_OFF:
-                            sendCallback(messageId, "Radio off");
-                            break;
+                    switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        sendCallback(messageId, "SMS sent");
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        sendCallback(messageId, "Generic failure");
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        sendCallback(messageId, "No service");
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        sendCallback(messageId, "Null PDU");
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        sendCallback(messageId, "Radio off");
+                        break;
                     }
                 }
             }, new IntentFilter(SENT));
 
-            //---when the SMS has been delivered---
-            reactContext.registerReceiver(new BroadcastReceiver(){
+            // ---when the SMS has been delivered---
+            reactContext.registerReceiver(new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context arg0, Intent arg1) {
-                    switch (getResultCode())
-                    {
-                        case Activity.RESULT_OK:
-                            System.out.println("SMS delivered");
-                            sendCallback(messageId, "SMS delivered");
-                            break;
-                        case Activity.RESULT_CANCELED:
-                            System.out.println("SMS not delivered");
-                            sendCallback(messageId, "SMS not delivered");
-                            break;
+                    switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        System.out.println("SMS delivered");
+                        sendCallback(messageId, "SMS delivered");
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        System.out.println("SMS not delivered");
+                        sendCallback(messageId, "SMS not delivered");
+                        break;
                     }
                 }
             }, new IntentFilter(DELIVERED));
 
-            SmsManager sms = SmsManager.getDefault();
+            // SmsManager sms = SmsManager.getDefault();
+
+            TelephonyManager telManager = (TelephonyManager) this.reactContext
+                    .getSystemService(Context.TELEPHONY_SERVICE);
+
+            SubscriptionManager manager = (SubscriptionManager) this.reactContext
+                    .getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+            List<SubscriptionInfo> subscriptionInfos = manager.getActiveSubscriptionInfoList();
+
+            SmsManager sms = SmsManager
+                    .getSmsManagerForSubscriptionId(subscriptionInfos.get(sim - 1).getSubscriptionId());
             sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
 
-        }catch (Exception e) {
+        } catch (Exception e) {
 
             sendCallback(messageId, "Unknown error");
             throw e;
